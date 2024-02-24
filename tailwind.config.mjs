4b2e181c57159f46b3db21b0d3b3c4636d5e405a
Jose Luis Parra Sánchez <jl.ps2289@gmail.com>
2024-02-24 06:53:40 -0500

    ✨ feature(setting): Clean files and fix build packages

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,23 @@ import animations from "@midudev/tailwind-animations";
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#fffee7",
+          100: "#fffdc1",
+          200: "#fff786",
+          300: "#ffea41",
+          400: "#ffd90d",
+          500: "#ecba00",
+          600: "#d19300",
+          700: "#a66802",
+          800: "#89510a",
+          900: "#74420f",
+          950: "#442204",
+        },
+      },
+    },
   },
   plugins: [animations],
 };
