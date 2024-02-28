66118ee1d1dd8dac99c2f4dc2a3063395fb2e26e
Jose Luis Parra Sánchez <jl.ps2289@gmail.com>
2024-02-27 22:18:43 -0500

    ✨ feature(cmd): Include command bar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,7 @@ import animations from '@midudev/tailwind-animations';
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'selector',
 	theme: {
 		extend: {
 			colors: {
